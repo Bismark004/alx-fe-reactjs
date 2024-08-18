467cadc8f1b8befb577b073f1a0f9c70ab13acee
marc-codes <adzaibismark@gmail.com>
2024-08-18 23:27:55 +0000

    commit

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <footer style={{ border: '1px solid white', background: 'white' , color: 'blue'}} >
+         <p>© 2023 City Lovers</p>
+        </footer>
+    )
+
+}
+export default Footer;
