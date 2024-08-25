079f2439522fcd8e9d438990f56288bca77f688f
marc-codes <adzaibismark@gmail.com>
2024-08-25 17:40:15 +0000

    commit

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,13 +2,15 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (id) => set((state) => ({ recipes: state.recipes.filter(recipe => recipe.id !== id) })),
-  updateRecipe: (updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map(recipe =>
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    ),
-  })),
+  searchTerm: '',
+  filteredRecipes: [],
+  setSearchTerm: (term) => set((state) => {
+    const lowerTerm = term.toLowerCase();
+    const filtered = state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(lowerTerm)
+    );
+    return { searchTerm: term, filteredRecipes: filtered };
+  }),
 }));
 
 export { useRecipeStore };
