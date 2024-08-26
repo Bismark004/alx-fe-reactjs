0fa4e3c5d5e6f7f52c5377cbdca2f1527056a567
marc-codes <adzaibismark@gmail.com>
2024-08-26 15:58:51 +0000

    commit

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,26 @@
 
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import { useRecipeStore } from '../recipeStore';
+import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
+  // Get the list of filtered recipes from the Zustand store
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
     <div>
-      {filteredRecipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>
-            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link> {/* Use Link for navigation */}
-          </h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h2>Recipe List</h2>
+      {filteredRecipes.length > 0 ? (
+        filteredRecipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link> {/* Link to recipe details */}
+            </h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>No recipes found.</p>
+      )}
     </div>
   );
 };
