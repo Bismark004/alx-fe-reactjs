8c8951415f0750ec5810a645419970aa602fce3b
marc-codes <adzaibismark@gmail.com>
2024-08-29 22:50:36 +0000

    commit

---
+++ react-query-demo/src/PostsComponents.jsx
@@ -0,0 +1,37 @@
+import { useQuery } from "react-query";
+
+const fetchPosts = async () => {
+    const response = await fetch('https://jsonplaceholder.typeicode.com/posts');
+    if(!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+}; 
+
+const PostsComponent = () => {
+    const {data, error, isLoading, isError} = useQuery('posts', fetchPosts);
+
+    if(isLoading) {
+        return <div>Loading...</div>
+
+    }
+
+    if(isError) {
+        return <div>{error.message}</div>
+
+    }
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                {data.map(post => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+
+
+};
+export default PostsComponent;
