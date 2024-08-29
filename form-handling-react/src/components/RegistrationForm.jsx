1d99c04642b40a393dd599a6499a7af555eb5dc8
marc-codes <adzaibismark@gmail.com>
2024-08-29 18:48:16 +0000

    commit

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,57 +1,62 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 const RegisterationForm = () => {
-    const [formData, setFormData] = useState( {username: '', email: '', password: ''});
-    const [error, setError] = useState('');
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
 
-     const handleChange = (e) => {
-        const {name, value} = e.target;
-        setFormData({ ...formData,
-           [name]: value});
-     }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUsername({...username, [name]: value});
+    setPassword({...password, [name]: value});
+    setEmail({...email, [name]: value});
+    
+  }
 
-     const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formData);
+const handleSubmit = (e) => {
+  e.preventDefault();
 
-        // Basic Validation
-        if (!formData.username || !formData.password || !formData.email) {
-          setError('All fields are required');
-          return
-        }
+  console.log(username, password, email); 
 
-        setError('');
-        console.log('Form Submitted', formData);
+  // Basic Validation
+  if (!username || !password || !email) {
+    setError('All fields are required');
+    return
+  }
 
-     }
-  
-    return(
-        <form  onSubmit={handleSubmit}>
-             {error && <p style={{color: 'red'}}>{error}</p>}
-            <input
-              type='text'
-              name='username'
-              value={formData.username}
-              onChange={handleChange}
-            />
+  setError('');
+  console.log('Form Submitted', username, password, email);
 
-            <input
-              type='email'
-              name='email'
-              value={formData.email}
-              onChange={handleChange}
-            />
+}
 
-            <input
-              type='password'
-              name='password'
-              value={formData.password}
-              onChange={handleChange}
-            />
+return (
+  <form onSubmit={handleSubmit}>
+    {error && <p style={{ color: 'red' }}>{error}</p>}
+    <input
+      type='text'
+      name='username'
+      value={username}
+      onChange={handleChange}
+    />
 
-            <button type='submit'>Submit</button>
-        </form>
+    <input
+      type='email'
+      name='email'
+      value={email}
+      onChange={handleChange}
+    />
 
-    )
+    <input
+      type='password'
+      name='password'
+      value={password}
+      onChange={handleChange}
+    />
+
+    <button type='submit'>Submit</button>
+  </form>
+
+)
 }
 export default RegisterationForm;
