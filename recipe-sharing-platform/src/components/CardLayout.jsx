056f02ea8cf3c64d5522090f21b572a99570cd7a
marc-codes <adzaibismark@gmail.com>
2024-09-11 01:25:39 +0000

    commit

--- recipe-sharing-platform/src/components/CardLayout.jsx
+++ recipe-sharing-platform/src/components/CardLayout.jsx
@@ -1,13 +1,30 @@
-const cardLayout = () => {
+const CardLayout = (props) => {
     const { title , summary , image} = props;
 
     return (
-        <div>
-            <img src={image} alt={title}/>
-            <h1>{title}</h1>
-            <p>{summary}</p>
+       
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                <div className="w-full bg-gray-900 rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row">
+                    <div className="w-full md:w-2/5 h-80">
+                      <img src={image} alt={title}  className="object-center object-cover w-full h-full"/>
 
-        </div>
+                    </div>
+
+                    <div className="w-full md:w-2/5 text-left p-6 md:p-4 space-y-2">
+                      <h1 className="text-xl text-white font-bold">{title}</h1>
+                      <p className="text-base leading-relaxed text-gray-500 font-normal">{summary}</p>
+                      
+
+                    </div>
+                 
+                  
+
+                </div>
+              
+            </div>
+           
+
+        
     )
 }
-export default cardLayout;
+export default CardLayout;
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,9 +1,50 @@
 import { useState, useEffect } from "react";
+import CardLayout from "./CardLayout";
+import recipeData from '../data.json';
 
-const HomePage () => {
-    const [recipe , setRecipe] = useState('');
+
+const HomePage = (props) => {
+    const [recipes , setRecipes] = useState('');
+
+    
+
+    useEffect(() => {
+        setRecipes(recipeData);
+    },[]);
+
+    const recipeCards = recipes.map((recipe) => (
+        <CardLayout
+        key={recipe.id}
+        title={recipe.title}
+        summary={recipe.summary}
+        image={recipe.image}
+    />
+
+    ));
+
+
+
     return (
+        <div className="w-full bg-gray-800">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+                <div className="text-center pb-12">
+                  <h1 className="text-base font-bold text-indigo-600">
+                    RECIPES
+                  </h1>
+                </div>
+
+                {recipeCards}
+
+              
+
+                 
+
+            </div>
+
+
+        </div>
 
     )
 }
 export default HomePage;
+
