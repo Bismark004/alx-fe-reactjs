647797b1bf4949dc6483c086e709d855e7f81b2e
marc-codes <adzaibismark@gmail.com>
2024-09-11 19:16:24 +0000

    commit

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -41,7 +41,7 @@ const HomePage = () => {
           <h1 className="text-base font-bold text-indigo-600">RECIPES</h1>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {recipeCards}
         </div>
       </div>
