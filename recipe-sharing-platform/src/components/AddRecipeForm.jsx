4086ee553ce15e0d3a1408ac7b170faad54b33f8
marc-codes <adzaibismark@gmail.com>
2024-09-12 16:26:21 +0000

    Create AddRecipeForm

---
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -0,0 +1,41 @@
+import {useForm} from 'react-hook-form';
+
+
+const AddRecipeForm = () => {
+    const {register, handleSubmit, formState:{ errors}} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+            <input
+            {...register("title",{required: true})}
+            className='border p-2 w-full rounded-lg'
+            placeholder='Recipe Title'
+            />
+            {errors.title && <span className='text-red-500'>Title is required</span>}
+
+            <input
+            {...register("ingredients",{required: true})}
+            className='border p-2 w-full rounded-lg'
+            placeholder='Ingredients (one per line)'
+            />
+            {errors.ingredients && <span className='text-red-500'>Title is required</span>}
+
+            <input
+            {...register("instructions",{required: true})}
+            className='border p-2 w-full rounded-lg'
+            placeholder='Preparation steps'
+            />
+            {errors.instructions && <span className='text-red-500'>Title is required</span>}
+
+
+
+
+
+        </form>
+    )
+}
+export default AddRecipeForm;
