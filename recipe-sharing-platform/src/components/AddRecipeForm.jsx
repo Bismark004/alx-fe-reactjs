21c6c710c4d56646e88ca3d58a2cdb55cd0ccd8c
marc-codes <adzaibismark@gmail.com>
2024-09-12 16:54:22 +0000

    commit

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -9,7 +9,7 @@ const AddRecipeForm = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+        <form onSubmit={handleSubmit(onSubmit)} className='max-w-lg mx-auto flex flex-col md:flex-row space-y-4'>
             <input
             {...register("title",{required: true})}
             className='border p-2 w-full rounded-lg'
@@ -17,19 +17,21 @@ const AddRecipeForm = () => {
             />
             {errors.title && <span className='text-red-500'>Title is required</span>}
 
-            <input
-            {...register("ingredients",{required: true})}
+            <textarea
+            {...register("ingredients",{
+                required: true,
+                validate: value => value.split('\n').lenght > 1 || "At least two ingredients are required"})}
             className='border p-2 w-full rounded-lg'
             placeholder='Ingredients (one per line)'
             />
-            {errors.ingredients && <span className='text-red-500'>Title is required</span>}
+            {errors.ingredients && <span className='text-red-500'>{errors.ingredients.message}</span>}
 
-            <input
+            <textarea
             {...register("instructions",{required: true})}
             className='border p-2 w-full rounded-lg'
             placeholder='Preparation steps'
             />
-            {errors.instructions && <span className='text-red-500'>Title is required</span>}
+            {errors.instructions && <span className='text-red-500'>instructions is required</span>}
 
 
             <button type="submit" className="bg-blue-500 text-white p-2 rounded-lg">Submit</button>
