af03c08b14b9abf4257999bdccf7b4da7f237a8d
marc-codes <adzaibismark@gmail.com>
2024-09-17 23:27:06 +0000

    Commit

--- github-user-search/src/components/SearchComponent.jsx
+++ github-user-search/src/components/SearchComponent.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+const SearchComponent = () => {
+    const [username, setUsername] = useState('');
+
+    const handlChange = (e) => {
+        setUsername(e.target.value);
+    }
 
 
-SearchComponent () {
+
+    
 
     return (
-        <form>
-            <input type="name" placeholder="enter username"/>
-        </form>
+        <div> 
+            <input 
+            type="name" 
+            placeholder="enter username"
+            name="username"
+            value={username}
+            onChange={handlChange}
+            />
+        </div>
     )
 }
+export default SearchComponent;
