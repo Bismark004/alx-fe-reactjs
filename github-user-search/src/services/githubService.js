f6a93f03e7fb00ae9446efa676c0dab8e0d36c94
marc-codes <adzaibismark@gmail.com>
2024-09-18 14:57:24 +0000

    commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,12 @@
-import axios  from "axios";
+
+import axios from 'axios';
 
 export const fetchUserData = async (username) => {
-    const response = await axios.get(`htttps://api.github.com/users/${username}`);
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
     return response.data;
-}
+  } catch (error) {
+    // Throwing an error to handle it in the Search component
+    throw new Error('User not found');
+  }
+};
